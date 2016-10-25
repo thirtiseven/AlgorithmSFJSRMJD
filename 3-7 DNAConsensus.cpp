b728fa91c418c293670393e192ba9a1636cda48c
thirtiseven lee <ntlihy@gmail.com>
2016-10-25 20:50:11 +0800

    add

--- 3-7 DNAConsensus.cpp
+++ 3-7 DNAConsensus.cpp
@@ -1,61 +1,59 @@
-/*
-#include <iostream>
 #include <cstdio>
-#include <string>
-#include <vector>
-#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-	int m,n;
-	scanf("%d %d",&m,&n);
-	char DNA[m][n];
-	for(int i = 0;i <= m;i++){
-		cin.getline(DNA[i],n,'\n');
+
+char alice[] = {' ','A','C','G','T'};
+char s[1037];
+int dave[1001][5];
+
+
+int bob(char clare){
+	switch (clare) {
+		case 'A':return 1;
+		case 'C':return 2;
+		case 'G':return 3;
+		case 'T':return 4;
+		default :return 0;
+		
 	}
-	for (int h = 0;h < m;h++){
-		for (int j = 0;j < n;j++){
-		cout << DNA[h][j];	
+}
+
+int main(int argc, char *argv[]){
+	int casenumber;
+	cin >> casenumber;
+	while (casenumber--) {
+		memset(dave,0,sizeof(dave));
+		
+		int m,n;
+		cin >> m >> n;
+		
+		for(int i=0;i<m;i++)
+		{
+			scanf("%s",s);
+		for(int j=0;j<n;j++)
+				dave[j][bob(s[j])]++;
 		}
+		
+		int eve , frank;
+		for(int p=0;p<n;p++){
+			eve = dave[p][1];
+			frank = 1;
+			for(int k=2;k<m;k++){
+				if(dave[p][k]>eve){
+					eve = dave[p][k];
+					frank = k;
+				}
+			}
+			cout << alice[frank];
+		}
+		
 	}
+}
 
-}*/
 
-#include <iostream>  
-#include<cstdio>   
-using namespace std;  
-struct DNA  
-{  
-	int a,c,g,t;  
-}d[1005];  
-int main(int argc, char *argv[])  
-{  
-	int t,n,m,i,j,ans,max;  
-	char s[1005];  
-	cin>>t;  
-	while(t--)  
-	{   scanf("%d%d",&n,&m);   
-		for(i=0;i<m;i++)  
-			d[i].a=d[i].c=d[i].g=d[i].t=0;  
-		for(i=0;i<n;i++)  
-		{scanf("%s",s);  
-		 for(j=0;j<m;j++)  
-		 if(s[j]=='A') d[j].a++;  
-			else if(s[j]=='C') d[j].c++;  
-				else if(s[j]=='G') d[j].g++;  
-						else d[j].t++;  
-		}  
-		for(ans=i=0;i<m;i++)  
-		{     
-			s[i]='A';max=d[i].a;  
-			if(d[i].c>max) s[i]='C',max=d[i].c;  
-			if(d[i].g>max) s[i]='G',max=d[i].g;  
-			if(d[i].t>max) s[i]='T',max=d[i].t;  
-			ans+=n-max;  
-		}  
-		s[m]=0;  
-		cout<<s<<endl<<ans<<endl;     
-	}  
-	return 0;  
-} 
+
+
